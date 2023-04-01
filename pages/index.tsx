6266dd4a15edaac6146cae9fed2ee55bacf0528b
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-04-01 20:54:51 +0530

    some more changes to landing page ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import BackGround from './bg.js'
+import SignUp from './signUp.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,6 +34,7 @@ export default function Home() {
       <div className={styles.bg}>
         <BackGround />
       </div>
+      <SignUp />
         
       </main>
 
