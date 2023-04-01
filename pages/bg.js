b3cd50d97f26f1cb48ce078140011a6c61a75d09
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-04-01 20:17:46 +0530

    changed the landing page design

---
+++ pages/bg.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css'
+
+const BackGround = () => {
+    return (
+        <div>
+            <h1 className={styles.hoverText}>Socify</h1>
+            <div className={styles.loadingBar}></div>
+        </div>
+        
+        
+    )
+}
+
+export default BackGround;
